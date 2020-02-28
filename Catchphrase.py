74c0daa52c89f5c3faed1c7bf7f400a303e99040
Yaru <juanestebannicolas@gmail.com>
2020-02-28 17:11:03 +0100

    Basic stuff
    Identify a path, get every srt file on that path, find every line which contains the desired word.

---
+++ Catchphrase.py
@@ -0,0 +1,32 @@
+import glob
+import os
+import sys
+
+
+def startreading():
+    if os.path.isdir(sys.argv[1]):
+        os.chdir(sys.argv[1])
+        target = sys.argv[2] #TODO: Multiple lines.
+        for file in glob.glob("*.srt"):
+            read(sys.argv[1], file, target)
+    else:
+        print("Not a valid path!")
+
+
+def saveline(line):
+    print("Not implemented yet")
+    return
+
+
+def read(path, file, target):
+    print(path, file)
+    openfile = open(file, 'r')
+    lines = openfile.readlines()
+    for line in lines:
+        if target in line:
+            saveline(line)
+        print(line.strip())
+    openfile.close()
+
+
+startreading()
