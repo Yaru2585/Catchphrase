5d674cc251be226e233fd427f9533a56f1a24284
Yaru <juanestebannicolas@gmail.com>
2020-02-28 17:33:08 +0100

    Saving lines on a file, and fixing stuff.

--- Catchphrase.py
+++ Catchphrase.py
@@ -3,30 +3,39 @@ import os
 import sys
 import re
 
+savedlines = []
+
 
 def startreading():
     if os.path.isdir(sys.argv[1]):
         os.chdir(sys.argv[1])
-        target = sys.argv[2] #TODO: Multiple lines.
+        target = sys.argv[2]  # TODO: Multiple lines.
         for file in glob.glob("*.srt"):
             read(sys.argv[1], file, target)
+        savelines()
+        print("Finished!")
+
     else:
         print("Not a valid path!")
 
 
-def saveline(line):
-    print("Not implemented yet")
-    return
+def savelines():
+    try:
+        outfile = open('result.txt', 'w')
+        outfile.writelines(savedlines)
+        outfile.close()
+    except Exception as e:
+        print("Something went wrong when saving the file: " + str(e))  # TODO: Custom exception.
 
 
 def read(path, file, target):
-    print(path, file)
     openfile = open(file, 'r')
     lines = openfile.readlines()
     for line in lines:
-        if re.search(target,line,re.IGNORECASE):
-            saveline(line)
-        print(line.strip())
+        if re.search(target, line, re.IGNORECASE):
+            ln = line.strip()
+            savedlines.append(ln + "\n")
+            print(ln)
     openfile.close()
 
 
